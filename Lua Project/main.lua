7b1725a81e893d99a1dc456a9375d2cce0d7387f
cfs27 <152335451+cfs27@users.noreply.github.com>
2025-05-08 01:05:56 -0400

    Moving red circle
    I made a red circle that moves using WASD with no boundaries.

--- Lua Project/main.lua
+++ Lua Project/main.lua
@@ -1,3 +1,23 @@
+-- attempting to make a simple circle that moves with WASD
+
+local x, y = 400, 300 -- inital position
+local speed = 200  -- pixels per sec
+
+function love.update(dt)
+    if love.keyboard.isDown("d") then
+        x = x + speed * dt -- move right when holding D
+    elseif love.keyboard.isDown("a") then
+        x = x - speed * dt -- move left when holding A
+    end
+
+    if love.keyboard.isDown("s") then 
+        y = y + speed * dt -- move down when holding S
+    elseif love.keyboard.isDown("w") then
+        y = y - speed * dt -- move up when holding W
+    end
+end
+
 function love.draw()
-    love.graphics.print("Wassgood", 300, 200)
+    love.graphics.setColor(0.6, 0.1, 0.1)
+    love.graphics.circle("fill", x, y, 30) -- the circle is filled in with a radius of 30 
 end
