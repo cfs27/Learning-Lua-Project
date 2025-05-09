8d97269cf607641b98194d4674628ee95d869eeb
cfs27 <152335451+cfs27@users.noreply.github.com>
2025-05-09 01:00:42 -0400

    Created movement boundaries at the edges of the window
    Created movement boundaries at the edges of the window to keep the circle from going out of the frame of view.

--- Lua Project/main.lua
+++ Lua Project/main.lua
@@ -2,22 +2,28 @@
 
 local x, y = 400, 300 -- inital position
 local speed = 200  -- pixels per sec
+local radius = 27
 
 function love.update(dt)
+    local windowWidth = love.graphics.getWidth()
+    local windowHeight = love.graphics.getHeight()
+-- Window dimensions
+
+-- Movement(now with boundaries at the windowWidth and windowHeight)
     if love.keyboard.isDown("d") then
-        x = x + speed * dt -- move right when holding D
+        x = math.min(x + speed * dt, windowWidth - radius)
     elseif love.keyboard.isDown("a") then
-        x = x - speed * dt -- move left when holding A
+        x = math.max(x - speed * dt, radius)
     end
 
     if love.keyboard.isDown("s") then 
-        y = y + speed * dt -- move down when holding S
+        y = math.min(y + speed * dt, windowHeight - radius)
     elseif love.keyboard.isDown("w") then
-        y = y - speed * dt -- move up when holding W
+        y = math.max(y - speed * dt, radius)
     end
 end
 
 function love.draw()
     love.graphics.setColor(0.6, 0.1, 0.1)
-    love.graphics.circle("fill", x, y, 30) -- the circle is filled in with a radius of 30 
+    love.graphics.circle("fill", x, y, 27) -- the circle is filled in with a radius of 27 
 end
